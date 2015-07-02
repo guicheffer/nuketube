99886f2e3a16b5191f2147b2f178071794311126
Joao Guilherme Cheffer Prado <j@guiatech.com.br>
2015-07-02 11:42:41 -0300

    Adjusted some configs on javascript

--- script.js
+++ script.js
@@ -14,37 +14,48 @@
 
 var nuketube = nuketube || {} ;
 
+var QueryString = function () {
+	var query_string = {};
+	var query = window.location.search.substring(1);
+	var vars = query.split("&");
+	for (var i=0;i<vars.length;i++) {
+		var pair = vars[i].split("=");
+		if (typeof query_string[pair[0]] === "undefined") {
+			query_string[pair[0]] = pair[1];
+		} else if (typeof query_string[pair[0]] === "string") {
+			var arr = [ query_string[pair[0]], pair[1] ];
+			query_string[pair[0]] = arr;
+		} else {
+			query_string[pair[0]].push(pair[1]);
+		}
+	} 
+	return query_string;
+} ();
+
 nuketube.config = {
-	copy_elements_context: 'copy-elements-for-nuketube',
-	// jquery_src_context: 'https://ajax.googleapis.com/ajax/libs/jquery/2.1.4/jquery.min.js',
+	html5_video_player: 'html5-video-player',
 	time_add_copies: 3000,
-
-	add_script_to_the_page: function( src, callback ){
-		var script = document.createElement( 'script' ) ;
-
-		script.setAttribute( 'src', src ) ;
-		script.setAttribute( 'type', 'text/javascript' ) ;
-
-		script.onload = callback ;
-		script.onreadystatechange = function() {
-			if ( this.readyState === 'loaded' || this.readyState === 'complete' ) {
-				callback() ;
-			}
-		} ;
-
-		document.getElementsByTagName('head')[ 0 ].appendChild( script ) ;
-	},
 
 	let_the_video_plays: function() {
 		var that = this,
-		copy_elements_context = document.createElement( 'div' ),
-		body = document.getElementById( 'body-container' ),
-		node = body.parentNode ;
+		body = document.getElementById( 'body' ),
+		body_container = document.getElementById( 'body-container' ),
+		head = document.getElementsByTagName( 'head' )[ 0 ],
+		html = document.getElementsByTagName( 'html' )[ 0 ] ;
 
+		head.parentNode.removeChild( head ) ;
+		body.parentNode.removeChild( body ) ;
+		body_new = document.createElement( 'body' ) ;
+		body_new.setAttribute( 'id', 'body' ) ;
+		body_new.style.background = '#000';
+		body_new.style.margin = '0';
+		clone_video_node = document.createElement( 'div' ) ;
+		clone_video_node.innerHTML = '<iframe width="100%" height="100%" style="position: absolute;" src="\
+			https://www.youtube.com/embed/' + QueryString.v + '?autoplay=1\
+			" frameborder="0" allowfullscreen autoplay></iframe>' ;
+		body_new.appendChild( clone_video_node ) ;
 
-		node.insertBefore( copy_elements_context, node ) ;
-		// $( '*:not(.html5-video-player):not(body)' ).appendTo( '.' + that.copy_elements_context ) ;
-		// $( 'video.html5-main-video', '.html5-video-player' ).css( 'width', '100%' ).css( 'height', 'auto' ) ;
+		html.appendChild( body_new ) ;
 	},
 
 	init: function() {
